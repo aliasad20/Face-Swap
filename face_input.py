cb6b34e3f56831a40b02ca25e1797d465468da7c
Ali Asad Quasim <76661857+aliasad20@users.noreply.github.com>
2021-02-05 01:08:15 +0530

    Delete face_input.py

--- face_input.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-src1 = input("Enter first image location") #for colelcting the source path of image with the full image name and extension written.
-src2 = input("Enter second image location") #for colelcting the source path of image with the full image name and extension written.
-img1 = cv2.imread(''+src1) #example to be entered in src: "C:\Users\user\Picturesa.jpg" or " /home/user/Pictures/a.jpg." 
-img2 = cv2.imread(''+src2) #example to be entered in src: "C:\Users\user\Picturesa.jpg" or " /home/user/Pictures/a.jpg." 
-cv2.imshow('Me',cv2.resize(img1, (640, 480))) #for showing and resizing the image being displayed
-cv2.waitKey(0) # waits for any key to be pressed.
-cv2.destroyAllWindows #closes the window in which image is displayed
